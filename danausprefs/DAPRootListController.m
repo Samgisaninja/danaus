d4b7ad1a412164564af178ae6c1aca764c81f8f5
Sam Gardner <samgisaninja@unc0ver.dev>
2020-05-30 16:48:08 -0500

    Actually include the prefs lol

---
+++ danausprefs/DAPRootListController.m
@@ -0,0 +1,39 @@
+#include "DAPRootListController.h"
+
+@implementation DAPRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+
+-(void)giveMeMoney{
+	NSDictionary *URLOptions = @{UIApplicationOpenURLOptionUniversalLinksOnly : @FALSE};
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.me/SamGardner4"] options:URLOptions completionHandler:nil];
+}
+
+- (void)openMyReddit{
+	NSDictionary *URLOptions = @{UIApplicationOpenURLOptionUniversalLinksOnly : @FALSE};
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.reddit.com/u/Samg_is_a_Ninja"] options:URLOptions completionHandler:nil];
+}
+
+-(void)openMyTwitter{
+	NSDictionary *URLOptions = @{UIApplicationOpenURLOptionUniversalLinksOnly : @FALSE};
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.twitter.com/Samg_is_a_Ninja"] options:URLOptions completionHandler:nil];
+}
+
+- (void)sendMail{
+	NSDictionary *URLOptions = @{UIApplicationOpenURLOptionUniversalLinksOnly : @FALSE};
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:samgisaninja@unc0ver.dev"] options:URLOptions completionHandler:nil];
+}
+
+- (void)openGithub{
+	NSDictionary *URLOptions = @{UIApplicationOpenURLOptionUniversalLinksOnly : @FALSE};
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.github.com/Samgisaninja/danaus/"] options:URLOptions completionHandler:nil];
+}
+
+@end
